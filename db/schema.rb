b61a7b4a0213865af56318b66da8fd1b718fffaf
Fabrizio Corbeta <fabriziocorbeta@gmail.com>
2026-08-18 08:20:50 -0300

    fix: agregar columna subtype a receivables
    Segundo hueco real de Task 1 (ademas de locked_attributes, ya commiteado
    en Task 2) -- TODA tabla accountable existente tiene subtype (agregada
    en su momento via add_subtype_to_accountables.rb para las 9 tablas
    originales), aunque varias no lo usen semanticamente (Property, Vehicle,
    etc). Account#subtype delega a accountable.subtype sin chequear que el
    metodo exista -- sin la columna, cualquier vista que renderice el
    sidebar de cuentas (via Account#short_subtype_label) rompia con
    NoMethodError al mostrar una cuenta Receivable.
    
    Descubierto al arreglar por separado un problema de infra de testing
    (assets Tailwind no precompilados bajo el bind-mount) que estaba
    enmascarando este error real.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_08_18_140000) do
+ActiveRecord::Schema[7.2].define(version: 2026_08_18_150000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -1281,6 +1281,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_08_18_140000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.jsonb "locked_attributes", default: {}
+    t.string "subtype"
   end
 
   create_table "recurring_transactions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
